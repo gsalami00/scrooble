56c49e79bfb5e46ffa36b0e855ca2e57221bd087
Michael <mmichaellin@gmail.com>
2018-08-08 16:35:19 -0400

    minor button redirect

--- client/components/Homepage.js
+++ client/components/Homepage.js
@@ -4,7 +4,7 @@ export default class Homepage extends Component {
   constructor() {
     super();
     this.state = {
-      name: ''
+      username: ''
     }
     this.handleChange = this.handleChange.bind(this)
     this.handleSubmit = this.handleSubmit.bind(this)
@@ -15,8 +15,9 @@ export default class Homepage extends Component {
     })
   }
   handleSubmit(event) {
-    event.preventDefault()
+   event.preventDefault()
     // do the firebase stuff here and then redirect to the game room
+    this.props.history.push('/game')
   }
   render() {
     return (
@@ -25,9 +26,9 @@ export default class Homepage extends Component {
         <div>Quick Play</div>
         <form onSubmit={this.handleSubmit} >
           <label htmlFor='username' >Player Name</label>
-          <input name="username" type='text' placeholder='Enter Name' value={this.state.name} onChange={this.handleChange} />
+          <input name="username" type='text' placeholder='Enter Name' value={this.state.username} onChange={this.handleChange} />
           <br />
-          <button type='submit'>Submit</button>
+          <button type='submit'>Play Now</button>
         </form>
       </div>
     )
