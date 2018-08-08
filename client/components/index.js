216151e1321e8f4b21f8736e806db96130c94a6f
Francisco Huergo <franhuergo@gmail.com>
2018-08-08 15:16:29 -0400

    created components and outline

---
+++ client/components/index.js
@@ -0,0 +1,24 @@
+/*
+
+COMPONENTS
+- Main (Click Here to Play)
+- Lobby (list of players on the left side)
+---Each player-card
+- Canvas
+---Hangman word ( _ _ _ )
+---Color picker
+---Timer
+---Round (#1, #2, #3) component
+---NavBar
+- Chat
+---Top
+---Messages
+---Input
+
+
+
+Phase 2
+- Login
+
+
+*/
