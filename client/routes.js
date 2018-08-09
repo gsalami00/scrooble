8a4233ad0c739002134fff685b7bb0c83f1eefa7
Geraldine Salamat <g.a.salamat@gmail.com>
2018-08-09 17:55:01 -0400

    canvas working:

--- client/routes.js
+++ client/routes.js
@@ -1,15 +1,17 @@
 import React, {Component} from 'react'
 import {Route, Switch} from 'react-router-dom'
-import {Homepage, Gameroom} from './components'
+import {Homepage, Gameroom, Canvas} from './components'
 import Messages from './components/chat/Messages'
-
 
 class Routes extends Component {
   render() {
     return (
       <Switch>
-        <Route exact path="/gameroom" component={Gameroom} />
+        <Route exact path="/canvas" component={Canvas} />
+        <Route path="/:roomId" component={Gameroom} />
+
         <Route exact path="/" component={Homepage} />
+
         {/* <Route exact path="/game" component={Messages} /> */}
       </Switch>
     )
