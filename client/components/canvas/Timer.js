b9e5ba7b7f3851827fa240baac7719fdc20b3c9d
leocrossman <lkcrossman@gmail.com>
2018-08-11 16:35:29 -0400

    some timer work

---
+++ client/components/canvas/Timer.js
@@ -0,0 +1,39 @@
+'use strict'
+
+import React, {Component} from 'react'
+import db from '../../../firestore.js'
+
+export default class Timer extends Component {
+  constructor() {
+    super()
+    this.state = {
+      time: 5,
+      start: false
+    }
+    this.countdown = this.countdown.bind(this)
+  }
+
+  componentDidMount() {
+    this.countdown()
+  }
+
+  countdown() {
+    let currTime = this.state.time
+    setInterval(() => {
+      if (currTime > -1) {
+        this.setState({
+          time: currTime--
+        })
+      }
+    }, 1000)
+  }
+
+  render() {
+    return (
+      <div>
+        hey hey
+        <h1>{this.state.time}</h1>
+      </div>
+    )
+  }
+}
