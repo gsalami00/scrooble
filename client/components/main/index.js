571ee40202fdaaab564890e29e4ea9a69ac6866f
Francisco Huergo <franhuergo@gmail.com>
2018-08-13 18:36:57 -0400

    where isFull is false

--- client/components/main/index.js
+++ client/components/main/index.js
@@ -9,7 +9,7 @@ export default class GameroomFinder extends Component {
         let notFullRooms = []
         await db
           .collection('rooms')
-          .where('isFull', '==', true) // change true to false after testing
+          .where('isFull', '==', false)
           .get()
           .then(querySnapshot => {
             querySnapshot.forEach(room => {
