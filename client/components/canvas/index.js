2d4cc60fb89a47b9d5526fb5d7b51d03a06125a9
Geraldine Salamat <g.a.salamat@gmail.com>
2018-08-13 18:27:26 -0400

    can draw line but not aligned with mouse and previous lines are connected with new ones

--- client/components/canvas/index.js
+++ client/components/canvas/index.js
@@ -14,12 +14,16 @@ export default class Canvas extends Component {
   constructor() {
     super()
     this.state = {
-      color: '#000000',
-      lineWidth: 6,
+      strokeColor: 'black',
+      lineWidth: 3,
       x: 0,
-      y: 0
+      y: 0,
+      record: false,
+      canvasData: []
     }
-    this.canvasData = []
+    this.handleMouseDown = this.handleMouseDown.bind(this)
+    this.handleMouseUp = this.handleMouseUp.bind(this)
+    this.handleMouseMove = this.handleMouseMove.bind(this)
   }
   async componentDidMount() {
     this.drawCanvas()
@@ -49,17 +53,65 @@ export default class Canvas extends Component {
     // })
   }
 
-  componentDidUpdate() {
-    this.drawCanvas()
+  // componentDidUpdate() {
+  //   this.drawCanvas()
+  // }
+  drawCanvas(start, end, strokeColor = 'black') {
+    const ctx = this.theCanvas.getContext('2d')
+    ctx.beginPath()
+    ctx.strokeStyle = strokeColor
+    ctx.moveTo(...start)
+    ctx.lineTo(...end)
+    ctx.closePath()
+    ctx.stroke()
   }
-
-  drawCanvas() {
-    const context = this.theCanvas.getContext('2d')
+  handleMouseDown(event) {
+    this.setState({
+      record: true
+    })
   }
+  handleMouseMove(event) {
+    if (this.state.record) {
+      console.log(this.state.canvasData)
+      const latestPoint = {
+        x: event.clientX,
+        y: event.clientY,
+        strokeColor: 'black',
+        size: 3
+      }
 
+      if (this.state.canvasData.length > 1) {
+        this.state.canvasData.forEach((point, idx, arr) => {
+          if (idx > 0 && idx < arr.length) {
+            let startX = arr[idx - 1].x
+            let startY = arr[idx - 1].y
+            let endX = point.x
+            let endY = point.y
+            this.drawCanvas(
+              [startX, startY],
+              [endX, endY],
+              arr[idx].strokeColor
+            )
+          }
+        })
+      }
+      this.setState({
+        canvasData: [...this.state.canvasData, latestPoint]
+      })
+    }
+  }
+  handleMouseUp() {
+    this.setState({
+      record: false
+    })
+  }
   render() {
     return (
-      <div>
+      <div
+        onMouseDown={this.handleMouseDown}
+        onMouseMove={this.handleMouseMove}
+        onMouseUp={this.handleMouseUp}
+      >
         <canvas
           ref={canvas => (this.theCanvas = canvas)}
           height={500}
