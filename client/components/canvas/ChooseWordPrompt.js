3db408963bb2a11c6e141d5e3c7dc9d60bdec76c
Francisco Huergo <franhuergo@gmail.com>
2018-08-14 11:41:35 -0400

    choosing word updates chooseWord in firebase for room

--- client/components/canvas/ChooseWordPrompt.js
+++ client/components/canvas/ChooseWordPrompt.js
@@ -6,7 +6,8 @@ export default class ChooseWordPrompt extends Component {
     super()
     this.state = {
       threeWords: [],
-      time: 12
+      time: 12,
+      roomId: localStorage.getItem('room')
     }
     this.handleSubmit = this.handleSubmit.bind(this)
   }
@@ -43,7 +44,18 @@ export default class ChooseWordPrompt extends Component {
   }
   async handleSubmit(word) {
     event.preventDefault()
-    alert(`the word is ${word}`)
+    await db
+      .collection('rooms')
+      .doc('HYHaIxc24e9R9jzNcJA9') // temporarily will have hard-coded room since component is currently outside of game while in development
+      .update({
+        chosenWord: word
+      })
+    const response = await db // this double-checks by grabbing the chosenWord from db instead of just what was clicked
+      .collection('rooms')
+      .doc('HYHaIxc24e9R9jzNcJA9') // temporarily will have hard-coded room since component is currently outside of game while in development
+      .get()
+    const chosenWord = response.data().chosenWord
+    alert(`The word has been updated to ${chosenWord}`)
     // then, tell (set) database that this word is chosenWord
   }
   countdown() {
