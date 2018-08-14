d43244459e18645f7bad73aaff81fc3da9d87a58
Michael <mmichaellin@gmail.com>
2018-08-14 11:36:27 -0400

    game room base styling

--- client/components/canvas/Timer.js
+++ client/components/canvas/Timer.js
@@ -29,11 +29,8 @@ export default class Timer extends Component {
   }
 
   render() {
-    return (
-      <div>
-        hey hey
-        <h1>{this.state.time}</h1>
-      </div>
-    )
+    return <div className="timer-text">
+    <div>{this.state.time}</div>
+    </div>
   }
 }
