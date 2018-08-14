9644f28673623bbb829b38558187fde4dd9eb78b
Geraldine Salamat <g.a.salamat@gmail.com>
2018-08-14 10:49:11 -0400

    working canvas without continuous lines

--- client/components/canvas/index.js
+++ client/components/canvas/index.js
@@ -1,12 +1,5 @@
 'use strict'
 
-/**
- * Creates a whiteboard on the page that the user can scribble on.
- *
- * Exports:
- *   - default draw(from, to, color, shouldBroadcast)
- *   - events: an EventEmitter that emits `draw` events.
- */
 import React, {Component} from 'react'
 import db from '../../../firestore.js'
 
@@ -14,12 +7,7 @@ export default class Canvas extends Component {
   constructor() {
     super()
     this.state = {
-      strokeColor: 'black',
-      lineWidth: 3,
-      x: 0,
-      y: 0,
       record: false,
-      lineEnd: false,
       canvasData: []
     }
     this.handleMouseDown = this.handleMouseDown.bind(this)
@@ -27,36 +15,26 @@ export default class Canvas extends Component {
     this.handleMouseMove = this.handleMouseMove.bind(this)
   }
   async componentDidMount() {
-    this.drawCanvas()
-    // const roomId = location.pathname.slice(1)
     const roomId = 'aQFOGkBUusXJmGP808XR' // REPLACE WITH LINE ABOVE
 
-    // const drawingInstance = await db
     const drawingCollectionInfo = await db
       .collection(`rooms/${roomId}/drawings`)
       .get()
-    console.log(drawingCollectionInfo)
+
     const drawingCollection = await db.collection(`rooms/${roomId}/drawings`)
-    console.log('Here is drawing collectioninfo!:', drawingCollectionInfo)
+
     if (drawingCollectionInfo.empty) {
-      console.log('drawing collection is empty! Time to add some stuff@!')
       drawingCollection.add({
         canvasData: [this.state]
       })
     } else {
       const drawingDoc = drawingCollectionInfo.docs[0].id
       await db.doc(`rooms/${roomId}/drawings/${drawingDoc}`).update({
-        canvasData: this.canvasData
+        canvasData: this.state.canvasData
       })
     }
-    // this.setState({
-    //   canvasData
-    // })
   }
 
-  // componentDidUpdate() {
-  //   this.drawCanvas()
-  // }
   drawCanvas(start, end, strokeColor = 'black') {
     const ctx = this.theCanvas.getContext('2d')
     ctx.beginPath()
@@ -66,60 +44,54 @@ export default class Canvas extends Component {
     ctx.closePath()
     ctx.stroke()
   }
-  handleMouseDown(event) {
+  handleMouseDown() {
     this.setState({
       record: true
     })
   }
   handleMouseMove(event) {
     if (this.state.record) {
-      // console.log(this.state.canvasData)
       const latestPoint = {
-        x: event.clientX,
-        y: event.clientY,
+        x: event.pageX - this.theCanvas.offsetLeft,
+        y: event.pageY - this.theCanvas.offsetTop,
         strokeColor: 'black',
-        size: 3,
+        lineWidth: 3,
         lineEnd: false
-      }
-
-      if (this.state.canvasData.length > 1) {
-        this.state.canvasData.forEach((point, idx, arr) => {
-          if (idx > 0 && idx < arr.length && !point.lineEnd) {
-            let startX = arr[idx - 1].x
-            let startY = arr[idx - 1].y
-            let endX = point.x
-            let endY = point.y
-            this.drawCanvas(
-              [startX, startY],
-              [endX, endY],
-              arr[idx].strokeColor
-            )
-          }
-        })
       }
       this.setState({
         canvasData: [...this.state.canvasData, latestPoint]
       })
+      this.state.canvasData.forEach((point, idx, arr) => {
+        if (idx > 0 && idx < arr.length && arr[idx - 1].lineEnd === false) {
+          let startX = arr[idx - 1].x
+          let startY = arr[idx - 1].y
+          let endX = point.x
+          let endY = point.y
+          this.drawCanvas(
+            [startX, startY],
+            [endX, endY],
+            point.strokeColor,
+            point.lineEnd
+          )
+        }
+      })
     }
   }
   handleMouseUp() {
+    let endDraw = this.state.canvasData[this.state.canvasData.length - 1]
+    endDraw.lineEnd = true
     this.setState({
       record: false,
-      canvasData: [
-        ...this.state.canvasData,
-        (this.state.canvasData[this.state.canvasData.length - 1][
-          lineEnd
-        ] = true)
-      ]
+      canvasData: [...this.state.canvasData, endDraw]
     })
   }
   render() {
-    console.log(this.state.record)
     return (
       <div
         onMouseDown={this.handleMouseDown}
         onMouseMove={this.handleMouseMove}
         onMouseUp={this.handleMouseUp}
+        onMouseOut={this.handleMouseUp}
       >
         <canvas
           ref={canvas => (this.theCanvas = canvas)}
