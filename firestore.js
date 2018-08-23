7ca2c6798599560e50eab31fe86290806af21909
Geraldine Salamat <g.a.salamat@gmail.com>
2018-08-23 16:18:26 -0400

    scrooble testing

--- firestore.js
+++ firestore.js
@@ -4,12 +4,12 @@ require('firebase/database')
 require('firebase/auth')
 
 firebase.initializeApp({
-  apiKey: 'AIzaSyAf2IueyuvP6v-LL9y_PsKC1kgZX9SYOi8',
-  authDomain: 'scrooble-710c9.firebaseapp.com',
-  databaseURL: 'https://scrooble-710c9.firebaseio.com',
-  projectId: 'scrooble-710c9',
-  storageBucket: 'scrooble-710c9.appspot.com',
-  messagingSenderId: '314296370194'
+  apiKey: 'AIzaSyCnnM_9_W86ZKde0TKVszMPAKEcPWf1FnM',
+  authDomain: 'scrooble-testing.firebaseapp.com',
+  databaseURL: 'https://scrooble-testing.firebaseio.com',
+  projectId: 'scrooble-testing',
+  storageBucket: 'scrooble-testing.appspot.com',
+  messagingSenderId: '767788557887'
 })
 const db = firebase.firestore()
 const settings = {timestampsInSnapshots: true}
@@ -18,3 +18,20 @@ db.settings(settings)
 export const fdb = firebase.database()
 
 export default db
+
+// USE THIS AS ORIGINAL
+// firebase.initializeApp({
+//   apiKey: 'AIzaSyAf2IueyuvP6v-LL9y_PsKC1kgZX9SYOi8',
+//   authDomain: 'scrooble-710c9.firebaseapp.com',
+//   databaseURL: 'https://scrooble-710c9.firebaseio.com',
+//   projectId: 'scrooble-710c9',
+//   storageBucket: 'scrooble-710c9.appspot.com',
+//   messagingSenderId: '314296370194'
+// })
+// const db = firebase.firestore()
+// const settings = {timestampsInSnapshots: true}
+// db.settings(settings)
+
+// export const fdb = firebase.database()
+
+// export default db
