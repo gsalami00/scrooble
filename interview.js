9f86a3fde77b8ec056d9b6959f83f0f00665e407
gsalami00 <36090313+gsalami00@users.noreply.github.com>
2018-09-02 12:45:39 -0400

    Delete interview.js

--- interview.js
+++
@@ -1,8 +0,0 @@
-let anchors = document.querySelectorAll('a')
-
-anchors.forEach(a => {
-  let spanEle = document.createElement('span')
-  spanEle.appendChild(a)
-  let parent = a.parentNode
-  parent.replaceChild(spanEle, a)
-})
